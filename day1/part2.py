4bf9cf97bf56d341408fd8b15c18cd4c72a6c0c2
Mitchell Peabody <mitchell.peabody@gmail.com>
2020-12-06 12:15:01 -0500

    Implement day 1 solution

---
+++ day1/part2.py
@@ -0,0 +1,21 @@
+import itertools
+import functools
+import operator
+
+NUMBER_OF_EXPENSES = 3
+
+with open("input1.txt", "r") as input_file:
+    expenses = [int(line.strip()) for line in input_file.readlines()]
+
+# This code makes an assumption that there will be only one combination
+# of expenses that sums to 2020. Since the itertools functions return
+# iterators, we can't simply index into the single element result, so
+# we use the next function to return it.
+valid_pair = next(itertools.
+                  filterfalse(
+                      lambda expenses: sum(expenses) != 2020,
+                      itertools.combinations(expenses, NUMBER_OF_EXPENSES)
+                  ))
+
+# Functools is so very very useful.
+print(functools.reduce(operator.mul, valid_pair, 1))
